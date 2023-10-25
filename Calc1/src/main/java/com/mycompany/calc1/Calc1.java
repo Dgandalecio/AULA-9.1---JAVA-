eff1b7826d66ac93ea76d2014e7878b043d48bb6
Dgandalecio <127789911+Dgandalecio@users.noreply.github.com>
2023-10-25 17:01:45 -0300

    Add files via upload

---
+++ Calc1/src/main/java/com/mycompany/calc1/Calc1.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.calc1;
+
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author diogo
+ */
+public class Calc1 {
+
+ public static double calcularExpressao(String expressao) {
+        Stack<Double> operandos = new Stack<>();
+        Stack<Character> operadores = new Stack<>();
+
+        StringTokenizer tokenizer = new StringTokenizer(expressao, " +-*/()", true);
+
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken().trim();
+
+            if (token.isEmpty()) {
+                continue;
+            }
+
+            if (isNumero(token)) {
+                operandos.push(Double.valueOf(token));
+            } else if (isOperador(token.charAt(0)) || token.equals("(")) {
+                operadores.push(token.charAt(0));
+            } else if (token.equals(")")) {
+                while (operadores.peek() != '(') {
+                    calcularOperacao(operandos, operadores);
+                }
+                operadores.pop();
+            }
+        }
+
+        while (!operadores.isEmpty()) {
+            calcularOperacao(operandos, operadores);
+        }
+
+        return operandos.pop();
+    }
+
+    public static boolean isNumero(String token) {
+        try {
+            Double.valueOf(token);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static boolean isOperador(char token) {
+        return "+-*/".indexOf(token) != -1;
+    }
+
+    public static void calcularOperacao(Stack<Double> operandos, Stack<Character> operadores) {
+        double b = operandos.pop();
+        double a = operandos.pop();
+        char operador = operadores.pop();
+        switch (operador) {
+            case '+' -> operandos.push(a + b);
+            case '-' -> operandos.push(a - b);
+            case '*' -> operandos.push(a * b);
+            case '/' -> operandos.push(a / b);
+        }
+    }
+
+    public static void main(String[] args) {
+        String expressaoInfixa;
+       try (Scanner scanner = new Scanner(System.in)) {
+           System.out.print("Digite a expressão a ser calculada: ");
+           expressaoInfixa = scanner.nextLine();
+       }
+
+        double resultado = calcularExpressao(expressaoInfixa);
+        System.out.println("Expressão Infixa: " + expressaoInfixa);
+        System.out.println("Resultado: " + resultado);
+    }
+
+    }
+
